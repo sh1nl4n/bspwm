76fad063c1966fc2718c479d9c6bc35fbc9011fb
snake <igoshin.rusl@yandex.ru>
2025-10-18 21:31:59 +0300

    step 6

--- dotfiles/polybar/launch.sh
+++ dotfiles/polybar/launch.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 pkill polybar && sleep 1
-polybar -c ~/.config/polybar/config.ini &
 
+export PRIMARY_MONITOR=$(xrandr --query | awk '/ primary/ {print $1; exit}')
+
+PRIMARY_MONITOR="$PRIMARY_MONITOR" polybar  -c ~/.config/polybar/config.ini   &
